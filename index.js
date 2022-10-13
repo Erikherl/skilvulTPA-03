974453a54fc61adea851e7dc6630396a3a0fb251
erikherl <erikherl7@gmail.com>
2022-10-13 23:15:23 +0700

    Adding API & Styling

---
+++ index.js
@@ -0,0 +1,52 @@
+const apiURL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=7e3fb35ea021dbf6b9e04db8d4ecf745'
+const imgURL = 'https://image.tmdb.org/t/p/w500'
+const searchURL = 'https://api.themoviedb.org/3/search/movie?api_key=7e3fb35ea021dbf6b9e04db8d4ecf745'
+
+const main = document.getElementById('main');
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+let lastUrl = '';
+
+getMovies(apiURL);
+
+function getMovies(url) {
+    lastUrl = url;
+    fetch(url).then(res => res.json()).then(data => {
+        console.log(data.results)
+        showMovies(data.results);
+    })
+}
+
+function showMovies(data) {
+    main.innerHTML = '';
+
+    data.forEach(movie => {
+        const {title, poster_path, vote_average, overview, id} = movie;
+        const movieEl = document.createElement('div');
+        movieEl.classList.add('movie');
+        movieEl.innerHTML = 
+        `
+            <img src="${poster_path? imgURL+poster_path: "http://via.placeholder.com/1080x1580" }" alt="${title}">
+            <div class="movie-info">
+                <h5>${title}</h5>
+                <span>${vote_average}</span>
+            </div>
+            <div class="overview">
+                <h3>Overview</h3>
+                ${overview}
+            </div>
+        `
+        main.appendChild(movieEl);
+    })
+}
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const searchTerm = search.value;
+
+    if(searchTerm){
+        getMovies(searchURL+'&query='+searchTerm)
+    }else{
+        getMovies(apiURL)
+    }
+})
